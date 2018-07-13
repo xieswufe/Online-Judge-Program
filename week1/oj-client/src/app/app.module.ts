940257677dee1ad53618a56e59c7d92f0da0f5cc
xieswufe <xieswufe@gmail.com>
2018-07-13 22:21:04 +0800

    update

--- week1/oj-client/src/app/app.module.ts
+++ week1/oj-client/src/app/app.module.ts
@@ -3,17 +3,26 @@ import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms'
 import {HttpModule} from '@angular/http';
 import { AppComponent } from './app.component';
-
+import { ProblemListComponent }  from './components/problem-list/problem-list.component';
+import { DataService } from './services/data.service';
+import { ProblemDetailComponent } from './components/problem-detail/problem-detail.component'
+import { routing } from "./app.routes";
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProblemListComponent,
+    ProblemDetailComponent
   ],
   imports: [
-    BrowserModule
-    FormsModule
-    HttpModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    routing
   ],
-  providers: [],
+  providers: [{
+    provide: "data",
+    useClass: DataService
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
